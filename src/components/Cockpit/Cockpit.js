574654afeae5560ea20c8008a618f2cda7f45e08
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-07 18:04:50 -0700

    React 16.2 Feature: Fragments and higher order function

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import classes from './Cockpit.css'
+import Aux from '../../hoc/Aux'
 
 export const cockpit = (props) => {
     const assignedClasses = []
 
-    let btnClass = ''
+    let btnClass = classes.Button
+
     if (props.showPersons) {
-        btnClass = classes.Red
+        btnClass = [classes.Button, classes.Red].join(' ')
     }
 
     if (props.persons.length <= 2) {
@@ -16,14 +18,14 @@ export const cockpit = (props) => {
         assignedClasses.push(classes.bold)
     }
     return(
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons
             </button>
-        </div>
+        </Aux>
     )
 }
 
