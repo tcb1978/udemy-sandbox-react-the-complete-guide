93b9ed53fd4185c70b910b210554e6e60e122abb
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-08 14:44:36 -0700

    add new ref on input in Person.js to add focus() in componentDidMount()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -19,6 +19,9 @@ class Person extends Component {
 
     componentDidMount = () => {
         console.log('[Person.js] Inside componentDidMount()')
+        if(this.props.postition === 0) {
+            this.inputElement.focus()
+        }
     }
     render() {
         console.log('[Person.js] Inside render()')
@@ -26,7 +29,11 @@ class Person extends Component {
             <Aux classes={classes.Person} >
                 <p onClick={this.props.click}>I'm a person and I am {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                    ref={(inp) => { this.inputElement = inp }}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
             </Aux>
         )
     }
